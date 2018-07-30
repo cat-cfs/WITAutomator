e0639079a2e1b33e678657b24c165a1c53b92bd1
Scott Morken <smorken@nrcan.gc.ca>
2018-07-30 15:39:37 -0700

    first commit

---
+++ WITAutomator/DBFLoader.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.IO;
+using System.Reflection;
+using System.Data;
+namespace WITAutomator
+{
+    public class DBFLoader
+    {
+        static string GetBlankDBPath()
+        {
+            string local_dir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            return Path.Combine(local_dir, "blank.accdb");
+        }
+
+        static DataTable ReadDBF(string dbfpath)
+        {
+            string dirname = Path.GetDirectoryName(dbfpath);
+            string filename = Path.GetFileName(dbfpath);
+            using (OleDbConnection con = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;" +
+                                        String.Format("Data Source={0}", dirname) +
+                                        "Extended Properties=dBase III")) {
+                OleDbCommand cmd = new OleDbCommand(String.Format("SELECT * FROM [{0}]", filename));
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                return dt;
+            }
+        }
+
+        static Dictionary<string, DataTable> ReadWoodStockDBFs(string dbf_dir)
+        {
+            //the standard woodstock table names/file names 
+            var woodstock_names = new Dictionary<string, string>()
+            {
+                { "Actions", "actions.dbf" },
+                { "Areas", "areas.dbf" },
+                { "Yeilds", "yields.dbf" },
+                { "Themes", "themes.dbf" },
+                { "Transitions", "trans.dbf" },
+                { "Schedule", "schedule.dbf" },
+            };
+
+            var output = new Dictionary<string, DataTable>();
+            foreach(var item in woodstock_names)
+            {
+                string path = Path.Combine("", )
+                output[item.Key] = ReadDBF(item.Value);
+            }
+            return output;
+        }
+        /// <summary>
+        /// loads the standard woodstock dbf files into an accdb format access database at the specified output path
+        /// </summary>
+        /// <param name="dbf_dir"></param>
+        /// <param name="db_output_path"></param>
+        /// <returns></returns>
+        static void LoadDBFFiles(string dbf_dir, string db_output_path)
+        {
+            File.Copy(GetBlankDBPath(), db_output_path);
+            string connection_string = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + db_output_path + ";";
+            using (OleDbConnection con = new OleDbConnection(connection_string))
+            {
+                Dictionary<string, DataTable> woodstock_dbfs = ReadWoodStockDBFs(dbf_dir);
+
+            }
+            
+
+        }
+    }
+}
---
+++ WITAutomator/WITImporter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using WoodStockImportTool;
+
+namespace WITAutomator
+{
+    class WITImporter
+    {
+        /// <summary>
+        /// import the woodstock dataset
+        /// </summary>
+        /// <param name="inputPath">path to access database containing the woodstock dbf data. 
+        /// The SIT input tables will be created in this database</param>
+        /// <param name="inputTables">list names of the tables to import</param>
+        /// <param name="roundingOption"></param>
+        public void Import(string inputPath, List<string> inputTables,
+            WoodStock.RoundingOption roundingOption = WoodStock.RoundingOption.MidPointRoundedUp)
+        {
+            WoodStock ws = new WoodStock();
+            ArrayList _inputTables = new ArrayList(inputTables);
+            ws.startImport(inputPath, roundingOption, _inputTables, false);
+        }
+    }
+}
